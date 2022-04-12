c2b7ca6890a7c30f28f98cfa078dc3fb49d813fc
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2022-04-12 14:35:37 +0200

    Update Docker, installation script

--- docker/wordpress/init.sh
+++ docker/wordpress/init.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+touch /tmp/shop.log
+
 # If we are in Github plugin repo CI environment
 CI_REPO_URL=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}
 if [[ ${CI_REPO_URL} == ${PLUGIN_URL//.git/} ]]; then
@@ -67,10 +69,15 @@ function install_plugin() {
   STR_PLUGIN=$(get_plugin.sh ${PLUGIN_URL} ${PLUGIN_VERSION})
   PLUGIN_NAME=$(echo ${STR_PLUGIN} | cut -d'^' -f1)
   PATH_TO_ZIP=$(echo ${STR_PLUGIN} | cut -d'^' -f2)
-  wp plugin install ${PATH_TO_ZIP} --activate  
+  wp plugin install ${PATH_TO_ZIP} --activate
+  wp wc --user=admin payment_gateway update qenta_checkout_page --enabled=true
 }
 
 function setup_store() {
+  wp theme install twentytwenty --activate
+  wp post delete 2 --force
+  wp post delete 1 --force
+  wp wc --user=admin tool run install_pages
   wp option set woocommerce_onboarding_opt_in "yes"
   wp option set woocommerce_onboarding_profile ""
   wp option set woocommerce_store_address "Store Street 11"
@@ -78,24 +85,53 @@ function setup_store() {
   wp option set woocommerce_store_city "Graz"
   wp option set woocommerce_store_postcode "8020"
   wp option set woocommerce_default_country "AT"
+  wp option set woocommerce_currency "EUR"
   wp_set_array woocommerce_onboarding_profile skipped 1
-  wp wc --user=admin tool run install_pages
   wp option update page_on_front 5
   wp option update show_on_front page
   wp option update blogdescription "QENTA Plugin DEMO"
-  wp theme install twentytwenty --activate
-  wp post delete 2 --force
+}
+
+function change_api_uri() {
+  PATH_CONF_QMORE='/var/www/html/wp-content/plugins/woocommerce-qenta-checkout-seamless/vendor/qenta/checkout-client-library/src/QentaCEE/QMore/Config/client.config.php'
+  PATH_CONF_QPAY='/var/www/html/wp-content/plugins/woocommerce-qenta-checkout-seamless/vendor/qenta/checkout-client-library/src/QentaCEE/QPay/Config/client.config.php'
+
+  DEFAULT_API_URI='https://api.qenta.com'
+
+  new_api_uri_protocol=$(sed -n 's,^\(https\?\):.\+,\1,p' <<< ${1})
+  new_api_uri_protocol=${new_api_uri_protocol:-https}
+  new_api_uri_hostname=$(sed -n 's,^\(https\?://\)\?\([^:/]\+\).*,\2,p' <<< ${1})
+  new_api_uri_port=$(sed -n 's,.\+:\([0-9]\+\).*,\1,p' <<< ${1})
+  # unused: path. must be configured then for every single entpoint
+  # new_api_uri_path=$(sed -n 's,\(https\?://\)\?[^/]\+\(.\+\),\2,p' <<< ${1})
+
+  new_api_uri_string=${new_api_uri_protocol}://
+  new_api_uri_string+=${new_api_uri_hostname}
+  [[ -n ${new_api_uri_port} ]] && new_api_uri_string+=':'${new_api_uri_port}
+
+  if [[ -z ${new_api_uri_hostname} ]]; then
+    echo "NOTE: API hostname unchanged. Invalid specification: ${1}"
+    return 1
+  fi
+
+  for config_file in ${PATH_CONF_QMORE} ${PATH_CONF_QPAY}; do
+    sed -i 's,'${DEFAULT_API_URI}','${new_api_uri_string}',g' ${config_file}
+  done
 }
 
 function print_info() {
   echo
   echo '####################################'
   echo
-  echo "URL: https://${WORDPRESS_URL}"
-  echo "Panel: https://${WORDPRESS_URL}/wp-admin/"
+  echo "Shop: https://${WORDPRESS_URL}"
+  echo "Admin Panel: https://${WORDPRESS_URL}/wp-admin/"
   echo "Plugin Config: https://${WORDPRESS_URL}/wp-admin/admin.php?page=wc-settings&tab=checkout"
   echo "User: ${WORDPRESS_ADMIN_USER}"
   echo "Password: ${WORDPRESS_ADMIN_PASS}"
+  if [[ ${_api_uri_changed} ]]; then
+    echo
+    echo "API Override: ${OVERRIDE_api_uri}"
+  fi
   echo
   echo '####################################'
   echo
@@ -122,9 +158,14 @@ else
     install_plugin
     _log "plugin installed"
   fi
+  if [[ -n ${OVERRIDE_api_uri} ]]; then
+    change_api_uri "${OVERRIDE_api_uri}" &&
+    _log "changed API URL to ${OVERRIDE_api_uri}" &&
+    _api_uri_changed=true
+  fi
 fi
 if [[ ${CI} != 'true' ]]; then
-  print_info
+  (sleep 1; print_info) &
 fi
 
 _log "url=https://${WORDPRESS_URL}"
